d71933001f78eaca2dca174e48c6f7886a63afe1
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2026-04-14 20:34:43 +0800

    Update lowcard.js

--- lowcard.js
+++ lowcard.js
@@ -3,12 +3,10 @@ main = "index.js"
 compatibility_date = "2026-04-14"
 
 [[durable_objects.bindings]]
-name = "CHAT_SERVER_2"
-class_name = "ChatServer2"
+name = "CHAT_SERVER_3"
+class_name = "ChatServer3"
 
 [[migrations]]
 tag = "v1"
-new_classes = ["ChatServer2"]
+new_classes = ["ChatServer3"]
 
-[limits]
-cpu_ms = 30
